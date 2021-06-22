f51fde76905ad9d188bcb828b97ec0a8f3679915
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-21 22:38:20 -0500

    Fixed lag

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -5,7 +5,8 @@ export default class Calculator extends Component {
         super()
         this.state = {
             num1: 0,
-            num2: 0
+            num2: 0,
+            sum: 0
         }
     }
 
@@ -13,14 +14,18 @@ export default class Calculator extends Component {
         this.setState({ [num]: e.target.value})
     }
 
+    findSum = (e) => {
+        this.setState({ sum: parseFloat(this.state.num1) + parseFloat(this.state.num2) })
+    }
+
     render() {
         return (
             <div class="container">
                 <h1>Add with React!</h1>
-
                 <div class="add">
                     <input type="number"
                         name="num1"
+                        step="any"
                         placeholder="Enter your first number"
                         value={this.state.num1}
                         onChange={(e) => this.setNum(e, 'num1')}
@@ -28,12 +33,14 @@ export default class Calculator extends Component {
                     <span>+</span>
                     <input type="number"
                         name="num2"
+                        step="any"
                         placeholder="Enter your second number"
                         value={this.state.num2}
                         onChange={(e) => this.setNum(e, 'num2')}
                     />
-                    <button>=</button>
+                    <button onClick={(e) => this.findSum(e)}>=</button>
                     <h3>Addition results go here!</h3>
+                    <span class="answer">{this.state.sum}</span>
                 </div>
             </div>
         )
