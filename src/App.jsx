c8d0a9d6ed2394991c2736f94d62683787142d92
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-21 19:08:30 -0500

    Initia, commit, step 1 complete.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './App.css'
+import Calculator from './Calculator'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    )
+  }
+}
---
+++ src/Calculator.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="container">
+            <h1>Add with React!</h1>
+
+            <div className="add">
+                <input type="text" />
+                <span>+</span>
+                <input type="text" />
+                <button>=</button>
+                <h3>Addition results go here!</h3>
+            </div>
+            </div>
+        )
+    }
+}
