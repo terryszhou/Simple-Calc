c975d4a4fcfb6f0c95d4634413a67f0401660479
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-22 09:17:40 -0500

    Created simple calc for all four basic math operators.

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -6,16 +6,23 @@ export default class Calculator extends Component {
         this.state = {
             num1: 0,
             num2: 0,
+            sign: "+",
             sum: 0
         }
     }
+
 
     setNum = (e, num) => {
         this.setState({ [num]: e.target.value})
     }
 
+    setSign = (e) => {
+        this.setState({ sign: e.target.value})
+        console.log(this.state.sign)
+    }
+
     findSum = (e) => {
-        this.setState({ sum: parseFloat(this.state.num1) + parseFloat(this.state.num2) })
+        this.setState({ sum: eval(`${parseFloat(this.state.num1)} ${this.state.sign} ${parseFloat(this.state.num2)}`) })
     }
 
     render() {
@@ -25,15 +32,20 @@ export default class Calculator extends Component {
                 <div class="add">
                     <input type="number"
                         name="num1"
-                        step="any"
                         placeholder="Enter your first number"
                         value={this.state.num1}
                         onChange={(e) => this.setNum(e, 'num1')}
                     />
-                    <span>+</span>
+                    {/* <span>+</span> */}
+                    <select value={this.state.sign} onChange={(e) => this.setSign(e)}>
+                        <option value="+">+</option>
+                        <option value="-">−</option>
+                        <option value="*">×</option>
+                        <option value="/">÷</option>
+                    </select>
+
                     <input type="number"
                         name="num2"
-                        step="any"
                         placeholder="Enter your second number"
                         value={this.state.num2}
                         onChange={(e) => this.setNum(e, 'num2')}
